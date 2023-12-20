2d8df399ab9b8079a16e71ad9c4dc16b0d042552
JackHarrison225 <Jh.rrison225@gmail.com>
2023-12-20 09:22:13 +0000

    Create pages and give them all a nav bar

--- MenuNav.js
+++ MenuNav.js
@@ -3,20 +3,14 @@ open = false
 function OpenClose()
 {
      if(open == true)
-     {
+     {    open = false;
           closeNav()
-          open = false;
-          document.getElementById("MenuButton").style.backgroundColor = "#3498db"
-          document.getElementById("NavBar").style.backgroundColor = "#3498db"
-          return document.getElementById("MenuButton").innerText = "Menu"
      }
      else if (open == false)
      {
-          openNav()
           open = true;
-          document.getElementById("MenuButton").style.backgroundColor = "#33333333"
-          document.getElementById("NavBar").style.backgroundColor = "#33333333"
-          return document.getElementById("MenuButton").innerText = "Close"
+          openNav()
+          
      }
 }
 
@@ -25,6 +19,9 @@ function openNav()
      document.getElementById("NavBar").style.width = "225px";
      document.getElementById("main").style.marginLeft = "225px";
      document.getElementById("main").style.marginRight = "-225px";
+     document.getElementById("MenuButton").style.backgroundColor = "#111111"
+     document.getElementById("NavBar").style.backgroundColor = "#111111"
+     return document.getElementById("MenuButton").innerText = "Close"
    
 }
   
@@ -33,5 +30,35 @@ function closeNav()
      document.getElementById("NavBar").style.width = "0";
      document.getElementById("main").style.marginLeft = "0";
      document.getElementById("main").style.marginRight = "0";
+     document.getElementById("MenuButton").style.backgroundColor = "#264f78"
+     document.getElementById("NavBar").style.backgroundColor = "#264f78"
+     return document.getElementById("MenuButton").innerText = "Menu"
 
+}
+
+
+function changeColour(){
+     if(open == true)
+     {
+          document.getElementById("MenuButton").style.backgroundColor = "#545454"
+          document.getElementById("NavBar").style.backgroundColor = "#545454"
+     }
+     else if (open == false)
+     {
+          document.getElementById("MenuButton").style.backgroundColor = "#9cdcfe"
+          document.getElementById("NavBar").style.backgroundColor = "#9cdcfe"
+     }
+}
+
+function changeColourBack(){
+     if(open == true)
+     {
+          document.getElementById("MenuButton").style.backgroundColor = "#111111"
+          return document.getElementById("NavBar").style.backgroundColor = "#111111"
+     }
+     else if (open == false)
+     {
+          document.getElementById("MenuButton").style.backgroundColor = "#264F78"
+          return document.getElementById("NavBar").style.backgroundColor = "#264f78"
+     }
 }
