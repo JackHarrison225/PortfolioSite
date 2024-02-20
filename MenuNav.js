5c23c181596cd30a65421f62d5480cade9a03e77
JackHarrison225 <jh.rrison225@gmail.com>
2024-02-20 14:42:03 +0000

    Projects Carasel added

--- MenuNav.js
+++ MenuNav.js
@@ -1,6 +1,38 @@
 //Navagation Bar
 open = false //used to see if navbar is open
 
+let ProjectIndex = 0
+const projectsList = [
+     {
+          projectName: "1",
+          projectFinished: true,
+          projectDescription: "",
+          projectLink: "",
+          projectImg: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQWXCbGVH7A7CpXlB0iih0uACBMzBU1vcqGz3TshlmqPe4jdNUqSLX8FKuJ_dY6el9zXds&usqp=CAU"
+     },
+     {
+          projectName: "2",
+          projectFinished: true,
+          projectDescription: "",
+          projectLink: "",
+          projectImg: ""
+     },
+     {
+          projectName: "3",
+          projectFinished: true,
+          projectDescription: "",
+          projectLink: "",
+          projectImg: ""
+     },
+     {
+          projectName: "4",
+          projectFinished: true,
+          projectDescription: "",
+          projectLink: "",
+          projectImg: ""
+     }
+]
+
 //open and close navbar
 function OpenClose()
 {
@@ -11,8 +43,7 @@ function OpenClose()
      else if (open == false)
      {
           open = true;
-          openNav()
-          
+          openNav()  
      }
 }
 
@@ -25,7 +56,6 @@ function openNav()
      document.getElementById("MenuButton").style.backgroundColor = "#111111"
      document.getElementById("NavBar").style.backgroundColor = "#111111"
      return document.getElementById("MenuButton").innerText = "Close"
-   
 }
 
 //everything that should be done when navbar closes
@@ -37,7 +67,6 @@ function closeNav()
      document.getElementById("MenuButton").style.backgroundColor = "#264f78"
      document.getElementById("NavBar").style.backgroundColor = "#264f78"
      return document.getElementById("MenuButton").innerText = "Menu"
-
 }
 
 //hover colour change
@@ -68,15 +97,60 @@ function changeColourBack(){
      }
 }
 
+//next carasel project
+function ChangeProjectUp(){
+     if(ProjectIndex + 1 > projectsList.length - 1)
+     {
+          ProjectIndex = 0
+          console.log(ProjectIndex)
+          SetProject()
+     }
+     else
+     {    
+          ProjectIndex += 1
+          console.log(ProjectIndex)
+          SetProject()
+     }
+}   
+
+function ChangeProjectDown(){      
+     if(ProjectIndex - 1 < 0)
+     {
+          ProjectIndex = projectsList.length - 1
+          console.log(ProjectIndex)
+          SetProject()
+     }
+     else
+     {    
+          ProjectIndex -= 1
+          console.log(ProjectIndex)
+          SetProject()
+     }
+}
 
 //event listeners for navbar menuw button
 document.getElementById("MenuButton").addEventListener("click", OpenClose)
 document.getElementById("MenuButton").addEventListener("mouseover", changeColour)
 document.getElementById("MenuButton").addEventListener("mouseleave", changeColourBack)
 
-
 //event listeners for Nar Bar Buttons
 document.getElementById("HomeButton").addEventListener("click", OpenClose)
 document.getElementById("ProjectButton").addEventListener("click", OpenClose)
-document.getElementById("PortfolioButton").addEventListener("click", OpenClose)
+//document.getElementById("PortfolioButton").addEventListener("click", OpenClose)
 document.getElementById("CVButton").addEventListener("click", OpenClose)
+
+//set values for slideshow
+function SetProject(){
+     document.getElementById("ProjectName").innerText = projectsList[ProjectIndex].projectName 
+     document.getElementById("ProjectFinished").innerText = `Finished: ${projectsList[ProjectIndex].projectFinished}` 
+     document.getElementById("ProjectDescription").innerText = projectsList[ProjectIndex].projectDescription 
+     document.getElementById("ProjectsCaraselBg").src = projectsList[ProjectIndex].projectImg
+}
+SetProject()
+//document.getElementById("ProjectLink").innerText = projectsList[ProjectIndex].projectLink 
+//document.getElementById("ProjectImg").innerText = projectsList[ProjectIndex].projectImg 
+document.getElementById("prev").innerText = "<"
+document.getElementById("next").innerText = ">"
+
+document.getElementById("prev").addEventListener("click", ChangeProjectDown)
+document.getElementById("next").addEventListener("click",  ChangeProjectUp)
